f4e2d74e1929cbaf981eac6ed633db40bd4acfac
Revath S Kumar <rsk@revathskumar.com>
2013-04-08 16:18:57 +0530

    adding tests

--- test/test-file-creation.js
+++ test/test-file-creation.js
@@ -1,24 +1,62 @@
-var assert = require("assert");
-var Generator = require("../all");
-var fs = require("fs");
-var path = require("path");
-var assert = require("assert");
-var yeoman = require('yeoman-generators').test;
+/*global describe:true, beforeEach:true, it:true */
+'use strict';
+var path    = require('path');
+var helpers = require('yeoman-generator').test;
+var assert  = require('assert');
 
-describe("BBB generator", function() {
-  before(yeoman.before(path.join(__dirname, 'temp')));
+describe('BBB generator', function () {
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, './temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+      this.backbone = {};
+      this.backbone.app = helpers.createGenerator('bbb:all', [
+        '../../all', [
+          helpers.createDummyGenerator(),
+          'mocha:app'
+        ]
+      ]);
+      done();
+    }.bind(this));
 
-  it ("should generate dotfiles", function() {
+  });
+
+  it('generator can be required without throwing', function () {
+    // not testing the actual run of generators yet
+    this.all = require('../all');
+  });
+
+  it('should generate dotfiles', function (done) {
     // FIXME: Remove the Gruntfile.js created by the test.before function
-    fs.unlinkSync("Gruntfile.js");
+    var expected = [
+      ['package.json', /"name": "temp"/],
+      'Gruntfile.js',
+      'app/404.html',
+      'app/favicon.ico',
+      'app/robots.txt',
+      'app/index.html',
+      'app/.htaccess',
+      '.gitignore',
+      '.gitattributes',
+      '.jshintrc',
+      '.editorconfig',
+      'Gruntfile.js',
+      'app/scripts/config.js',
+      'app/scripts/app.js',
+      'app/scripts/main.js',
+      'app/scripts/router.js'
+    ];
 
-    var g = new Generator();
-    g.run();
+    helpers.mockPrompt(this.backbone.app, {
+      'compassBootstrap': 'Y',
+      'includeRequireJS': 'N'
+    });
 
-    yeoman.assertFile(".bowerrc");
-    yeoman.assertFile(".gitignore");
-    yeoman.assertFile(".editorconfig");
-    yeoman.assertFile(".jshintrc");
+    this.backbone.app.run({}, function () {
+      helpers.assertFiles(expected);
+      done();
+    });
 
   });
 
